afcd614b6221b8711f7ef7d8d54ebae6faad26e0
almamatterhit <87114490+almamatterhit@users.noreply.github.com>
2021-07-08 13:38:08 +0530

    Add files via upload

---
+++ hit/day5/ArithmeticDemo3.java
@@ -0,0 +1,26 @@
+package hit.day5;
+
+public class ArithmeticDemo3 {
+	public static void main(String[] args) {
+		int i=10;
+		
+		//i=i+10;
+		i+=10;
+		System.out.println(i);//print 20
+		i=10;
+		i-=10;
+		System.out.println(i);//print 0
+		i=10;
+		i*=10;
+		System.out.println(i);//print 100
+		
+		i=10;
+		i/=10;
+		System.out.println(i);//print 1
+		
+		i=10;
+		i%=10;
+		System.out.println(i);//print 0
+		
+	}
+}
